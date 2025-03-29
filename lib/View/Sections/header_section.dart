74b3935aacdac48de25a87b70e6c998b10ed148f
Moiz <khanmoaiz682@gmail.com>
2025-03-29 05:11:26 +0500

    Add deprecation ignore comments in multiple files to enhance code clarity

--- lib/View/Sections/header_section.dart
+++ lib/View/Sections/header_section.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
